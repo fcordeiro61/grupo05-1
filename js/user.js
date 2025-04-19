beb172fb062fbbb73fc54d42d567b5a577b7038d
Fabio Cordeiro <fcordeiro6@gmail.com>
2025-04-19 18:23:09 -0300

    rebase

--- js/user.js
+++
@@ -1,7 +0,0 @@
-// scripts/user.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Página de usuários carregada');
-    // Aqui você pode carregar os usuários via fetch e gerar os cards dinamicamente.
-  });
-  
---
+++ src/js/user.js
@@ -0,0 +1,16 @@
+<<<<<<< HEAD:js/user.js
+// scripts/user.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página de usuários carregada');
+    // Aqui você pode carregar os usuários via fetch e gerar os cards dinamicamente.
+  });
+=======
+// scripts/user.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página de usuários carregada');
+    // Aqui você pode carregar os usuários via fetch e gerar os cards dinamicamente.
+  });
+>>>>>>> f72d479 (Atualizaçao de estrutura de diretorios e docker):src/js/user.js
+  
