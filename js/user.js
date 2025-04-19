900f3f0dbee8e530617c1b7debc4b27902d009cc
Fabio Cordeiro <fcordeiro6@gmail.com>
2025-04-19 01:33:03 -0300

    atualizações na aplicação e prints do prototipo

---
+++ js/user.js
@@ -0,0 +1,7 @@
+// scripts/user.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Página de usuários carregada');
+    // Aqui você pode carregar os usuários via fetch e gerar os cards dinamicamente.
+  });
+  
